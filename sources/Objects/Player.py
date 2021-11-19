f410c6df94626c81e84b26104f1d04d14793240a
SeungWon Seo <ttd8591@gmail.com>
2021-11-19 15:24:34 +0900

    change player image

--- sources/Objects/Player.py
+++ sources/Objects/Player.py
@@ -10,18 +10,17 @@ class Player(Object.Object):
         self.width = 44
         self.height = 20
         self.delta_time = 0
-        self.real_x = self.x + 35
+        self.real_x = self.x
         self.health = 5
         self.attack_damage = 1
 
         # player image
-        self.spr_idle = pygame.image.load("../sprites/player/idle.png").convert_alpha()         # 0
-        self.spr_move = pygame.image.load("../sprites/player/move.png").convert_alpha()         # 1
-        self.spr_attack1 = pygame.image.load("../sprites/player/attack1.png").convert_alpha()   # 2
-        self.spr_attack2 = pygame.image.load("../sprites/player/attack2.png").convert_alpha()   # 3
-        self.spr_attack3 = pygame.image.load("../sprites/player/attack3.png").convert_alpha()   # 4
-        self.spr_hit = pygame.image.load("../sprites/player/hit.png").convert_alpha()           # 5
-        self.spr_death = pygame.image.load("../sprites/player/death.png").convert_alpha()       # 6
+        self.spr_idle = pygame.image.load("../sprites/HeroKnight/Idle.png").convert_alpha()         # 0
+        self.spr_move = pygame.image.load("../sprites/HeroKnight/Run.png").convert_alpha()          # 1
+        self.spr_dash = pygame.image.load("../sprites/HeroKnight/Dash.png").convert_alpha()         # 2
+        self.spr_attack = pygame.image.load("../sprites/HeroKnight/Attack.png").convert_alpha()     # 3
+        self.spr_hit = pygame.image.load("../sprites/HeroKnight/Hit.png").convert_alpha()           # 4
+        self.spr_death = pygame.image.load("../sprites/HeroKnight/Death.png").convert_alpha()       # 5
 
         # player sound
         self.sound_walk = pygame.mixer.Sound("../sounds/player/walk.mp3")
@@ -49,7 +48,7 @@ class Player(Object.Object):
         self.is_move_able = True
 
         # player attack
-        self.attack_range = 120
+        self.attack_range = 130
         self.attack_combo = 0
         self.attack_delay = 0
         self.attack_max_delay = 1000
@@ -87,11 +86,11 @@ class Player(Object.Object):
         self.state_index = 0
 
         # sprite information
-        self.spr_width = 88
-        self.spr_height = 30
+        self.spr_width = 140
+        self.spr_height = 140
         self.spr_speed = 80
         self.spr_index = 0
-        self.spr_size = 3
+        self.spr_size = 2
         self.spr_list = []
         self.spr_x = self.x + self.spr_width / 2 * self.spr_size
         self.spr_y = self.x + self.spr_height / 2 * self.spr_size
@@ -119,44 +118,38 @@ class Player(Object.Object):
     def set_sprite(self):
         lis = []
         # state is idle
-        for i in range(0, 6):
-            lis.append(self.spr_idle.subsurface(0, i * self.spr_height, self.spr_width, self.spr_height))
+        for i in range(0, 11):
+            lis.append(self.spr_idle.subsurface(i * self.spr_width, 0, self.spr_width, self.spr_height))
         self.spr_list.append(lis[:])
         lis.clear()
 
         # state is move
         for i in range(0, 8):
-            lis.append(self.spr_move.subsurface(0, i * self.spr_height, self.spr_width, self.spr_height))
+            lis.append(self.spr_move.subsurface(i * self.spr_width, 0, self.spr_width, self.spr_height))
         self.spr_list.append(lis[:])
         lis.clear()
 
-        # state is attack1
-        for i in range(0, 10):
-            lis.append(self.spr_attack1.subsurface(0, i * self.spr_height, self.spr_width, self.spr_height))
+        # state is dash
+        for i in range(0, 4):
+            lis.append(self.spr_dash.subsurface(i * self.spr_width, 0, self.spr_width, self.spr_height))
         self.spr_list.append(lis[:])
         lis.clear()
 
-        # state is attack2
-        for i in range(0, 8):
-            lis.append(self.spr_attack2.subsurface(0, i * self.spr_height, self.spr_width, self.spr_height))
-        self.spr_list.append(lis[:])
-        lis.clear()
-
-        # state is attack3
-        for i in range(0, 9):
-            lis.append(self.spr_attack3.subsurface(0, i * self.spr_height, self.spr_width, self.spr_height))
+        # state is attack
+        for i in range(0, 6):
+            lis.append(self.spr_attack.subsurface(i * self.spr_width, 0, self.spr_width, self.spr_height))
         self.spr_list.append(lis[:])
         lis.clear()
 
         # state is hit
-        for i in range(0, 6):
-            lis.append(self.spr_hit.subsurface(0, i * self.spr_height, self.spr_width, self.spr_height))
+        for i in range(0, 4):
+            lis.append(self.spr_hit.subsurface(i * self.spr_width, 0, self.spr_width, self.spr_height))
         self.spr_list.append(lis[:])
         lis.clear()
 
         # state is death
-        for i in range(0, 5):
-            lis.append(self.spr_death.subsurface(0, i * self.spr_height, self.spr_width, self.spr_height))
+        for i in range(0, 9):
+            lis.append(self.spr_death.subsurface(i * self.spr_width, 0, self.spr_width, self.spr_height))
         self.spr_list.append(lis[:])
 
     def draw_image(self):
@@ -165,13 +158,13 @@ class Player(Object.Object):
             # if current index over than max index
             if math.floor(self.spr_index) > len(self.spr_list[self.state_index]) - 1:
                 # if player attack
-                if 2 <= self.state_index and self.state_index <= 4:
+                if self.state_index == 3:
                     self.state_index = 0
                     self.is_attack_able = True
                     self.is_move_able = True
                     self.is_attacking = False
                 # if player hit
-                elif self.state_index == 5:
+                elif self.state_index == 4:
                     self.state_index = 0
                     self.is_move_able = True
                 self.spr_index = 0
@@ -227,15 +220,12 @@ class Player(Object.Object):
 
                 if keys[pygame.K_RIGHT]:
                     self.x += self.move_speed * self.delta_time
-
                     if self.direction:
-                        self.x = self.x + self.spr_width
                         self.direction = False
                         self.attack_range *= -1
                 if keys[pygame.K_LEFT]:
                     self.x -= self.move_speed * self.delta_time
                     if not self.direction:
-                        self.x = self.x - self.spr_width
                         self.direction = True
                         self.attack_range *= -1
             else:
@@ -261,26 +251,12 @@ class Player(Object.Object):
             # check combo
             if self.attack_delay >= self.attack_max_delay:
                 self.attack_delay = 0
-                self.attack_combo = 0
 
             # player attack
             if self.is_attack_able:
                 self.spr_index = 0
-
-                if self.attack_combo == 0:
-                    self.state_index = 2
-                    self.attack_combo = 1
-                    pygame.mixer.Sound.play(self.sound_attack1)
-
-                elif self.attack_combo == 1:
-                    self.state_index = 3
-                    self.attack_combo = 2
-                    pygame.mixer.Sound.play(self.sound_attack2)
-
-                elif self.attack_combo == 2:
-                    self.state_index = 4
-                    self.attack_combo = 0
-                    pygame.mixer.Sound.play(self.sound_attack3)
+                self.state_index = 3
+                pygame.mixer.Sound.play(self.sound_attack1)
 
                 self.is_move_able = False
                 self.is_attack_able = False
@@ -295,14 +271,14 @@ class Player(Object.Object):
                 self.is_hit_able = True
 
             if self.is_hit_able:
-                self.state_index = 5
+                self.state_index = 4
                 self.spr_index = 0
                 self.health -= damage
                 self.is_hit_able = False
                 self.is_move_able = False
 
             if self.health <= 0:
-                self.state_index = 6
+                self.state_index = 5
                 self.spr_index = 0
                 self.is_player_death = True
 
